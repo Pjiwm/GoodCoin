c69a13b06bb76ea7f5bd60e5ec0c8b16238fa035
Pjiwm <pim.munne@gmail.com>
2024-01-05 02:05:12 +0100

    removed unused file

--- src/test_t.py
+++
@@ -1,7 +0,0 @@
-import ipaddress
-import socket
-me = "python_goodcoin"
-other = "client2_goodcoin"
-# print(ipaddress.IPv4Address(me))
-# print(ipaddress.IPv4Address(other))
-print(socket.gethostbyname(socket.gethostname()))
